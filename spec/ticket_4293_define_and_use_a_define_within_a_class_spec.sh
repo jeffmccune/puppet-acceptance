1d0520b00d7030786089c57c4f18c5d936dffa45
Jeff McCune <jeff@puppetlabs.com>
2010-07-22 18:23:23 -0700

    Added test for [#4293] define something and use it
    This test appears to only come up in agent/server
    mode.  I could not trigger the bug in 2.6.0rc4
    using stand alone apply mode.
    
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

---
+++ spec/ticket_4293_define_and_use_a_define_within_a_class_spec.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# 2010-07-22 Jeff McCune <jeff@puppetlabs.com>
+#
+# AffectedVersion: 2.6.0rc4
+# FixedVersion: 2.6.0
+#
+# Description: using a defined type in the class it's declared in
+# causes an error.
+#
+
+set -u
+source spec/setup.sh
+set -e
+
+OUTPUT=/tmp/puppet-$$.output
+MANIFEST=/tmp/puppet-$$-master/manifests/site.pp
+
+driver_master_and_agent_locally_using_old_executables
+
+puppet_conf <<'CONF'
+[main]
+  logdir=$vardir/log
+  ssldir=$vardir/ssl
+[puppetd]
+  certname=puppetclient
+  report=true
+[puppetmasterd]
+  reports=store
+CONF
+
+mkdir -p "$(dirname ${MANIFEST})"
+cat <<'PP' >"${MANIFEST}"
+class foo {
+  define do_notify($msg) {
+    notify { "Message for $name: $msg": }
+  }
+  do_notify { 'test_one': msg => 'a_message_for_you' }
+}
+node default {
+  include foo
+}
+PP
+
+puppetmasterd \
+  --vardir /tmp/puppet-$$-master-var \
+  --confdir /tmp/puppet-$$-master \
+  --rundir /tmp/puppet-$$-master \
+  --no-daemonize --autosign=true \
+  --verbose --debug --color false \
+  --certname=localhost --masterport 18140 2>&1 >"${OUTPUT}" &
+master_pid=$!
+
+# Wait for the master port to be availalbe
+wait_until_master_is_listening $master_pid
+
+start_puppetd
+
+killwait ${master_pid}
+
+if grep -q a_message_for_you "${OUTPUT}"; then
+  exit $EXIT_OK
+else
+  exit $EXIT_FAILURE
+fi
+
